cde576ad88c163043685510bb2cd559e31b73688
Noah Pollock <pollocknoah@gmail.com>
2016-05-16 19:50:42 +0100

    user sign-up specs passing

--- spec/feature/web_helper.rb
+++ spec/feature/web_helper.rb
@@ -1,8 +1,11 @@
-def sign_up
+def sign_up (name: 'Noah',
+             email: 'toplad@mate.com',
+             password: '1234',
+             password_confirmation: '1234')
   visit  '/users/new'
-  fill_in :name, with: 'Noah'
-  fill_in :email, with: 'toplad@mate.com'
-  fill_in :password, with: '1234'
-  fill_in :password_confirmation, with: '1234'
+  fill_in :name, with: name
+  fill_in :email, with: email
+  fill_in :password, with: password
+  fill_in :password_confirmation, with: password_confirmation 
   click_button 'Sign Up'
 end
