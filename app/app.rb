d7c594459b3fb4b2338ea927e2e38e8a48f771f5
Noah Pollock <pollocknoah@gmail.com>
2016-05-17 09:51:01 +0100

    User Sign Up

--- app/app.rb
+++ app/app.rb
@@ -2,7 +2,7 @@ ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
 require_relative 'dm_setup'
-# require 'sinatra/partial'
+## require 'sinatra/partial'
 
 require_relative 'server'
 require_relative 'controllers/users'
