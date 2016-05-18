7f7f45626981ff55d7d4a12b1ee189aff24be04c
Michael Harrison <harrim91@hotmail.co.uk>
2016-05-18 22:41:46 +0100

    can add inventory dates for an accommodation if logged in as accommodation owner

--- app/controllers/inventories.rb
+++ app/controllers/inventories.rb
@@ -1,8 +1,14 @@
 class MakersBNB < Sinatra::Base
 
   post '/inventories' do
-    (params[:start_date]..params[:end_date]).each do |date|
-      Inventory.create(date: date,booked: nil, accommodation_id: params[:accommodation_id])
+    start_date = DateTime.strptime(params[:start_date],"%d/%m/%Y")
+    end_date = DateTime.strptime(params[:end_date],"%d/%m/%Y")
+    (start_date..end_date).each do |date|
+      Inventory.create(date: date,
+                       booked: nil,
+                       accommodation_id: params[:accommodation_id])
     end
+    flash.next[:notice] = 'Inventory sucessfully added'
+    redirect back
   end
 end
