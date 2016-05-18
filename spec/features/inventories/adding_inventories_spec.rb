f363bbc1e4bafa66b14722b7869a7dfa075a14d9
Michael Harrison <harrim91@hotmail.co.uk>
2016-05-18 22:49:21 +0100

    added extra tests for access rights

--- spec/features/inventories/adding_inventories_spec.rb
+++ spec/features/inventories/adding_inventories_spec.rb
@@ -7,6 +7,13 @@ feature 'adding inventories' do
                 password_confirmation: 'password')
   end
 
+  let(:user2) do
+    User.create(name: 'Dan',
+                email: 'dan@test.com',
+                password: 'password',
+                password_confirmation: 'password')
+  end
+
   let(:accommodation) do
     Accommodation.create(name: 'Michael\'s House',
                          desc: 'Cool place',
@@ -14,16 +21,35 @@ feature 'adding inventories' do
                          user_id: user.id)
   end
 
-  context 'logged in' do
+  context 'logged in as accommodation owner' do
     before do
       login email: user.email, password: user.password
     end
 
-    scenario 'a range of dates can be picked' do
+    scenario 'a range of dates can be added' do
       visit "accommodations/#{accommodation.id}"
       fill_in :start_date, with: "10/12/2016"
       fill_in :end_date, with: "11/12/2016"
       expect{ click_button('Add Dates') }.to change(Inventory, :count).by(2)
+      expect(page).to have_content 'Inventory sucessfully added'
+    end
+  end
+
+  context 'logged in as different user' do
+    before do
+      login email: user2.email, password: user2.password
+    end
+
+    scenario 'no dates can be added' do
+      visit "accommodations/#{accommodation.id}"
+      expect(page).not_to have_css('form#new-inventories')
+    end
+  end
+
+  context 'not logged in' do
+    scenario 'no dates can be added' do
+      visit "accommodations/#{accommodation.id}"
+      expect(page).not_to have_css('form#new-inventories')
     end
   end
 end
