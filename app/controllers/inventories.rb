c92a58de6afb4abfbf589820b9ab133e87e381a3
Michael Harrison <harrim91@hotmail.co.uk>
2016-05-19 23:45:48 +0100

    fixing datepicker

--- app/controllers/inventories.rb
+++ app/controllers/inventories.rb
@@ -1,5 +1,11 @@
 class MakersBNB < Sinatra::Base
 
+  get '/inventories/:id' do
+    headers 'Access-Control-Allow-Origin' => '*'
+    inventory = Inventory.all(accommodation_id: params[:id])
+    inventory.to_json
+  end
+
   post '/inventories' do
     start_date = DateTime.strptime(params[:start_date],"%d/%m/%Y")
     end_date = DateTime.strptime(params[:end_date],"%d/%m/%Y")
--- app/public/scripts/script.js
+++ app/public/scripts/script.js
@@ -1,3 +1,52 @@
 $(document).ready(function() {
-  $('.datepicker').datepicker({dateFormat: 'dd/mm/yy'});
+
+  var accomPath = new RegExp(/\/accommodations\/\d+/);
+  var inventory = [];
+
+  function setMinForEndDate(date) {
+    $('.end-date').datepicker("option", "minDate", date);
+  }
+
+  function setInventory(data) {
+    inventory = data;
+  }
+
+  if (window.location.pathname.match(accomPath)) {
+    var accomIDMatcher = new RegExp(/\d+/)
+    var accomID = window.location.pathname.match(accomIDMatcher);
+    $.getJSON('/inventories/'+accomID[0], function(data) {
+      setInventoryDates(data);
+    });
+  }
+
+  $('.start-date').datepicker({
+    dateFormat: 'dd/mm/yy',
+    minDate: new Date,
+    onSelect: setMinForEndDate
+  });
+
+  $('.end-date').datepicker({
+    dateFormat: 'dd/mm/yy',
+    minDate: new Date
+  });
+
+  function hideDate(date) {
+    datesToHide = []
+    if($.inArray($.datepicker.formatDate('dd/mm/yy', datesToHide), datesToHide) > -1) {
+      return [false];
+    } else {
+      return [true];
+    }
+  }
+
+    function showDate(date) {
+    datesToHide = []
+    if($.inArray($.datepicker.formatDate('dd/mm/yy', datesToShow), datesToShow) > -1) {
+      return [true];
+    } else {
+      return [false];
+    }
+  }
+
 });
+
