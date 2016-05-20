df745551283d8aa807c77d3b69909ea399cafdf1
Michael Harrison <harrim91@hotmail.co.uk>
2016-05-20 12:29:11 +0100

    finished datepicker

--- app/public/scripts/script.js
+++ app/public/scripts/script.js
@@ -1,51 +1,99 @@
 $(document).ready(function() {
-
-  var accomPath = new RegExp(/\/accommodations\/\d+/);
   var inventory = [];
+  var inventoryDates = [];
+  var availableInventoryDates = [];
+  var accomPath = new RegExp(/\/accommodations\/\d+/);
 
   function setMinForEndDate(date) {
-    $('.end-date').datepicker("option", "minDate", date);
+    $('#end-date, #check-out').datepicker("option", "minDate", date);
   }
 
   function setInventory(data) {
     inventory = data;
   }
 
+  function setInventoryDates() {
+    for (i=0; i<inventory.length; i++) {
+      inventoryDates.push(formatDate(inventory[i].date));
+    }
+  }
+
+  function setAvailableInventoryDates() {
+    for (i=0; i<inventory.length; i++) {
+      if(inventory[i].booked === false) {
+        availableInventoryDates.push(formatDate(inventory[i].date));
+      }
+    }
+  }
+
   if (window.location.pathname.match(accomPath)) {
-    var accomIDMatcher = new RegExp(/\d+/)
+    var accomIDMatcher = new RegExp(/\d+/);
     var accomID = window.location.pathname.match(accomIDMatcher);
     $.getJSON('/inventories/'+accomID[0], function(data) {
-      setInventoryDates(data);
+      setInventory(data);
+      setInventoryDates();
+      setAvailableInventoryDates();
     });
   }
 
-  $('.start-date').datepicker({
+  $('#start-date').datepicker({
     dateFormat: 'dd/mm/yy',
     minDate: new Date,
-    onSelect: setMinForEndDate
+    onSelect: setMinForEndDate,
+    beforeShowDay: hideInventoryDates
   });
 
-  $('.end-date').datepicker({
+  $('#end-date').datepicker({
     dateFormat: 'dd/mm/yy',
-    minDate: new Date
+    minDate: new Date,
+    beforeShowDay: hideInventoryDates
   });
 
-  function hideDate(date) {
-    datesToHide = []
-    if($.inArray($.datepicker.formatDate('dd/mm/yy', datesToHide), datesToHide) > -1) {
+  $('#check-in').datepicker({
+    dateFormat: 'dd/mm/yy',
+    minDate: new Date,
+    onSelect: setMinForEndDate,
+    beforeShowDay: showInventoryDates
+  });
+
+  $('#check-out').datepicker({
+    dateFormat: 'dd/mm/yy',
+    minDate: new Date,
+    beforeShowDay: showInventoryDates
+  });
+
+  function hideInventoryDates(date) {
+    if($.inArray($.datepicker.formatDate('dd/mm/yy', date), inventoryDates) > -1) {
       return [false];
     } else {
       return [true];
     }
   }
 
-    function showDate(date) {
-    datesToHide = []
-    if($.inArray($.datepicker.formatDate('dd/mm/yy', datesToShow), datesToShow) > -1) {
+  function showInventoryDates(date) {
+    if($.inArray($.datepicker.formatDate('dd/mm/yy', date), availableInventoryDates) > -1) {
       return [true];
     } else {
       return [false];
     }
+  }
+
+  function formatDate(date) {
+    var dateObj = new Date(date)
+    var day = dateObj.getDate();
+    var month = dateObj.getMonth() + 1;
+    var year = dateObj.getFullYear();
+
+    if (day < 10) {
+        day = "0" + day;
+    }
+    if (month < 10) {
+        month = "0" + month;
+    }
+
+    var formattedDate = day + "/" + month + "/" + year;
+
+    return formattedDate;
   }
 
 });
