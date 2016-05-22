e89230edd45ce70cf7a1d754f539c2f2b7197f5e
Noah Pollock <pollocknoah@gmail.com>
2016-05-22 16:24:52 +0100

    CSS update

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -3,7 +3,7 @@ class Inventory
 
   property :id, Serial
   property :date, Date, unique_index: :accommodation_date
-  property :booked, Boolean, unique_index: :accommodation_date
+  property :booked, Boolean
 
   belongs_to :accommodation
   validates_uniqueness_of :date, scope: :accommodation
